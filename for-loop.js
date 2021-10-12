79649a0f050ab52e6c1b8ac31ab47b60f848bb63
BoNeZ4Lyfe <NUR.MAHAD@GMAIL.COM>
2021-10-12 06:02:37 -0400

    Javascripting lessons done so far

---
+++ for-loop.js
@@ -0,0 +1,18 @@
+var total = 0
+var limit = 10
+for (let i= 0; i < limit; i++) {
+  total += i
+  
+}
+console.log(total)
+
+/*  let total = 0
+const limit = 10
+    
+for (let i = 0; i < limit; i++) {
+  total += i
+}
+
+console.log(total)
+*/ 
+
